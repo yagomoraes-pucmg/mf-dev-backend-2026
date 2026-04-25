51ab6e5ab8cb637c30af6bc0fdbc0be1ce60f845
Yago Moraes <yago.moraes@sga.pucminas.br>
2026-04-25 20:07:12 -0300

    Criação dos métodos CRUD

--- mf-dev-backend-2026/Models/Veiculo.cs
+++ mf-dev-backend-2026/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2026.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        [Display(Name ="Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        [Display(Name = "Ano de Modelo")]
         public int AnoModelo { get; set; }
     }
 }
