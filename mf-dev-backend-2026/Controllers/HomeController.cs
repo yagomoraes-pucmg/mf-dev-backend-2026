cb3942f7b77ada74cd86b68856410166fd788990
Yago Rodrigues <yago.dmoraes@gmail.com>
2026-04-25 17:45:32 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-backend-2026/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using mf_dev_backend_2026.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace mf_dev_backend_2026.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
