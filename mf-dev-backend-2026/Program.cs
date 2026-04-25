0f25c1d9d6161935bcb54b727d80fb332677a833
Yago Moraes <yago.moraes@sga.pucminas.br>
2026-04-25 18:31:07 -0300

    Configurando Entity Framework

--- mf-dev-backend-2026/Program.cs
+++ mf-dev-backend-2026/Program.cs
@@ -1,7 +1,13 @@
+using mf_dev_backend_2026.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
